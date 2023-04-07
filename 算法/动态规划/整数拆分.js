1affea6515d9dc2ad14e2db2b503aefe5d5d506a
StaRain <1945159551@qq.com>
2023-04-07 21:39:58 +0800

    代码提交

--- 算法/动态规划/整数拆分.js
+++ 算法/动态规划/整数拆分.js
@@ -1,10 +1,13 @@
 var integerBreak = function (n) {
-    const dp = [1]
+    const dp = [0, 1]
 
 
     for (let i = 2; i < n; i++) {
         for (let j = 1; j < i; j++) {
-            dp[i] = j * dp[i - j];
+            if (dp[i] === null) {
+                console.log(1)
+                dp[i] = j * dp[i - j];
+            }
         }
     }
     return dp
