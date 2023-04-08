c70d4e7e5fd6798d4da4195a3eb98a9abfc95a41
StaRa1n <1945159551@qq.com>
2023-04-08 10:10:07 +0800

    提交代码

--- 11.js
+++
@@ -1,2 +0,0 @@
-const dp = Array(3).fill().map(item => Array(7))
-console.log(dp)
--- 算法/动态规划/整数拆分.js
+++ 算法/动态规划/整数拆分.js
@@ -1,13 +1,11 @@
 var integerBreak = function (n) {
-    const dp = [0, 1]
+    let dp = Array(n + 1).fill(0)
+    dp[2] = 1;
 
 
-    for (let i = 2; i < n; i++) {
-        for (let j = 1; j < i; j++) {
-            if (dp[i] === null) {
-                console.log(1)
-                dp[i] = j * dp[i - j];
-            }
+    for (let i = 3; i <= n; i++) {
+        for (let j = 1; j <= i / 2; j++) {
+            dp[i] = Math.max(j * (i - j), j * dp[i - j], dp[i])
         }
     }
     return dp
