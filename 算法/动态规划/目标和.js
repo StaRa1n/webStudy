0f040a25cdb91c7cd90c260e73279354987bd1fb
StaRain <1945159551@qq.com>
2023-04-13 20:53:07 +0800

    4.13

--- 算法/动态规划/目标和.js
+++ 算法/动态规划/目标和.js
@@ -3,20 +3,21 @@ var findTargetSumWays = function (nums, target) {
     // x = (sum + target) / 2
     const sum = nums.reduce((pre, cur) => pre + cur)
     let x = (sum + target) / 2
-    if (x & 1) return 0
+    if ((target + sum) % 2) return 0
 
     const dp = Array(x + 1).fill(0)
+    dp[0] = 1
 
     for (let i = 0; i < nums.length; i++) {
-        for (let j = target + 1; j > sum[i]; j--) {
+        for (let j = x; j >= nums[i]; j--) {
             dp[j] += dp[j - nums[i]]
         }
     }
 
-    return dp[dp.length - 1]
+    return dp
 };
 
 
-const arr = [1, 1, 1, 1, 1]
+const arr = [1]
 
-console.log((findTargetSumWays(arr, 3)));
+console.log((findTargetSumWays(arr, 1)));
